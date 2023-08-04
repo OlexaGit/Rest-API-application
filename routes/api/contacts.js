917e5141744f72a49274ee296038d8141c0e229a
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-08-05 01:52:35 +0300

    setting and send get reqest

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,38 @@
-const express = require('express')
+const fs = require("fs/promises");
+const path = require("path");
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await fs.readFile(
+      path.join(__dirname, "..", "..", "models", "contacts.json"),
+      "utf-8"
+    );
+    res.send(JSON.parse(data));
+  } catch (error) {
+    console.error(error);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.status(500).send("Internul server error");
+  }
+  // res.json({ message: "template message" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
