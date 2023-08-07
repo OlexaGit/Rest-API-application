ede6bbff2285637ad8ba9b9126eddf21956f79e6
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-08-07 17:35:19 +0300

    added middleware

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
 
 const router = express.Router();
 
@@ -8,10 +10,10 @@ router.get("/", ctrl.listContacts);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.addContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
 router.delete("/:contactId", ctrl.removeContact);
 
-router.put("/:contactId", ctrl.updateContact);
+router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateContact);
 
 module.exports = router;
