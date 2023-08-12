2b3e55eb8ecead96399f11995974bbb0a7cfca15
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-08-13 00:15:18 +0300

    add .env

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-
-const DB_HOST =
-  "mongodb+srv://Oleksii:27HYAYmfX2qr45hw@cluster0.5rdcghc.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
     console.log("Database connect successfull");
