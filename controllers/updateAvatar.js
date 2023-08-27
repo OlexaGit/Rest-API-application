6db918d8150958b777cf69a45a9280ee705405c8
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-08-27 15:49:35 +0300

    added functiom change size avatar

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -3,21 +3,23 @@ const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
 
+const { HttpError } = require("../helpers");
+
 const avatarsDir = path.join(__dirname, "..", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-  await Jimp.read(originalname)
-    .then((avatar) => {
-      return avatar.resize(250, 250).write("avatar.png");
-    })
-    .catch((err) => console.log(err));
-  console.log(originalname);
+
+  const changeSizeAvatar = await Jimp.read(tempUpload);
+  if (!changeSizeAvatar) {
+    throw HttpError(400);
+  }
+  changeSizeAvatar.resize(250, 250).write(tempUpload);
 
   const fileName = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, fileName);
-  // console.log(resultUpload);
+  console.log("tempUpload", tempUpload);
 
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", fileName);
