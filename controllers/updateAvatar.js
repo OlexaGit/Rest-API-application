af99c6a0110385cd4f2a85d507241254be430a83
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-08-27 13:43:30 +0300

    added Jimp

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -1,14 +1,24 @@
 const { User } = require("../models/user");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "..", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+  await Jimp.read(originalname)
+    .then((avatar) => {
+      return avatar.resize(250, 250).write("avatar.png");
+    })
+    .catch((err) => console.log(err));
+  console.log(originalname);
+
   const fileName = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, fileName);
+  // console.log(resultUpload);
+
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", fileName);
   await User.findByIdAndUpdate(_id, { avatarURL });
