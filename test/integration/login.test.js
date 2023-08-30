00e33fbc4bbfac9b217b4c3c13901ddd172bf37f
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-08-30 14:02:47 +0300

    made tast for login

--- test/integration/login.test.js
+++ test/integration/login.test.js
@@ -2,91 +2,43 @@ require("dotenv").config();
 
 const supertest = require("supertest");
 const mongoose = require("mongoose");
-const { app } = require("./../../app");
+const app = require("./../../app");
 const { User } = require("../../models/user");
 
 mongoose.set("strictQuery", false);
 
-const { DB_HOST } = process.env;
+const { DB_TEST_URI } = process.env;
 
 describe("login", () => {
   beforeAll(async () => {
-    await mongoose.connect(DB_HOST);
-
-    // await supertest(app).get("/register").send({
-    //   email: "testUser1@gmail.com",
-    //   password: "123456",
-    // });
-
-    // await supertest(app).post("/api/auth/register").send({
-    //   email: "User1@example.com",
-    //   password: "1examplepassword",
-    // });
-
-    // await User.deleteMany();
+    await mongoose.connect(DB_TEST_URI);
+    await User.deleteMany();
   });
 
   afterAll(async () => {
-    await mongoose.disconnect(DB_HOST);
+    await mongoose.disconnect(DB_TEST_URI);
   });
 
-  //   it("should login user", async () => {
-  //     const response = await supertest(app).post("/api/auth/login").send({
-  //       email: "testUser1@gmail.com",
-  //       //   email: "User1@example.com",
-  //       password: "123456",
-  //       //   password: "1examplepassword",
-  //     });
-
-  //     expect(response.statusCode).toBe(200);
-  //     // expect(response.body.data.user.email).toBe("testUser1@gmail.com");
-  //     expect(response.body.token).toBe(token);
-  //     expect(response.body.User.email).toBe("testUser1@gmail.com");
-  //     expect(response.body.User.subscription).toBe("starter");
-  //   });
-
-  //   it("should register new user", async () => {
-  //     const response = await supertest(app).post("/api/auth/register").send({
-  //       email: "testUser1@gmail.com",
-  //       password: "123456",
-  //     });
-  //     console.log(response);
-  //     expect(response.statusCode).toBe(201);
-  //     expect(response.body.data.user.email).toBe("testUser1@gmail.com");
-  //   });
-
-  it("should register new user", async () => {
+  it("should register new user for login", async () => {
     const response = await supertest(app).post("/api/auth/register").send({
-      email: "User1@example.com",
+      email: "testUser1@gmail.com",
       password: "1examplepassword",
     });
 
-    expect(response.statusCode).toBe(201);
-    expect(response.body.data.user.email).toBe("testUser1@gmail.com");
+    expect(201);
+    expect(response.body.User.email).toBe("testUser1@gmail.com");
+    expect(response.body.User.subscription).toBe("starter");
   });
 
-  // *****
-  //   it("should register new user", async () => {
-  //     const response = await supertest(app).post("/api/auth/register").send({
-  //       email: "testUser1@gmail.com",
-  //       password: "123456",
-  //     });
-  //
-  //     expect(response.statusCode).toBe(201);
-  //     expect(response.body.data.user.email).toBe("testUser1@gmail.com");
-  //   });
+  it("should login user", async () => {
+    const response = await supertest(app).post("/api/auth/login").send({
+      email: "testUser1@gmail.com",
+      password: "1examplepassword",
+    });
 
-  //   it("should not register the same user 2 times", async () => {
-  //     await supertest(app).post("/api/auth/register").send({
-  //       email: "testUser2@gmail.com",
-  //       password: "123456",
-  //     });
-
-  //     const response = await supertest(app).post("/api/auth/register").send({
-  //       email: "testUser2@gmail.com",
-  //       password: "123456",
-  //     });
-
-  //     expect(response.statusCode).toBe(409);
-  //   });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.token).toBe(response.body.token);
+    expect(response.body.User.email).toBe("testUser1@gmail.com");
+    expect(response.body.User.subscription).toBe("starter");
+  });
 });
